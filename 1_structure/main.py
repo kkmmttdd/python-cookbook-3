68cfc14a10f908585df4b402e0d7507b8c8e886f
kkmmttdd <kkmmttdd@gmail.com>
2020-12-26 21:51:40 +0900

    priority queue

--- 1_structure/main.py
+++ 1_structure/main.py
@@ -35,3 +35,39 @@ def search(f: TextIO, pattern: str) -> deque:
 with open('some.txt', 'r') as f:
     for current, s in search(f, 'python'):
         print(current, s)
+
+
+print('============ 1.5 prioty queue')
+
+import heapq
+
+class PQ:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        self._index += 1
+        heapq.heappush(self._queue, (-priority, self._index, item))
+
+    def pop(self):
+        i = heapq.heappop(self._queue)[-1]
+        print(i)
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+
+h = PQ()
+h.push(Item('sadffda'), 1)
+
+h.push(Item('asdf'), 3)
+h.push(Item('wwwww'), 2)
+h.pop()
+h.pop()
+h.pop()
